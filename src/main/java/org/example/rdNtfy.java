2867ee4ae72d21a07cd6c7fc6ff7046a946b3ec6
attil <at@gml.com>
2026-06-23 03:27:11 +0800

    fix bug

---
+++ src/main/java/org/example/rdNtfy.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.File;
+//。。。
+public class rdNtfy {
+
+    public static void main(String[] args) {
+        listFiles("/storage/emulated/0/Android/data/com.aaapkg/files");
+    }
+
+    //列出文件
+    private static void listFiles(String dir) {
+        File file = new File(dir);
+        if (!file.exists()) {
+            System.out.println("目录不存在: " + dir);
+            return;
+        }
+
+        if (file.isFile()) {
+            System.out.println(file.getAbsolutePath());
+            return;
+        }
+
+        File[] files = file.listFiles();
+        if (files == null) {
+            return;
+        }
+
+        for (File f : files) {
+            if (f.isDirectory()) {
+                listFiles(f.getAbsolutePath());
+            } else {
+                System.out.println(f.getAbsolutePath());
+            }
+        } }
+}
